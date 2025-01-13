8f359d0416df229de7cd3a508b3e35ac2f49c3e0
SastreMax <sastremaximiliano@gmail.com>
2025-01-12 22:26:09 -0300

    button-FOOTER-PACKAGE-
    . Se identa el codigo del componente button.
    . Se importan heroicons para el footer. Se modifica.
    . Se agregan dependencias para los heroicons.

--- components/Footer.js
+++ components/Footer.js
@@ -1,28 +1,20 @@
+import { FaceSmileIcon, EnvelopeIcon, PhoneIcon } from "@heroicons/react/24/solid";
+
 export function Footer() {
     return (
         <footer className="footer">
             <p>&copy; 2024 Productos Latam, propiedad de Maximiliano Sastre. Todos los derechos reservados.</p>
-            <div className="redes">
-                <a href="https://es-la.facebook.com/" title="Facebook" target="_blank" rel="noopener noreferrer">
-                    <i className="fa-brands fa-facebook fa-beat"></i>
+            <div className="redes flex justify-center space-x-4 mt-4">
+                <a href="https://facebook.com" title="Facebook">
+                    <FaceSmileIcon className="h-6 w-6 text-white hover:text-blue-400" />
                 </a>
-                <a href="https://www.whatsapp.com/?lang=es_LA" title="WhatsApp" target="_blank" rel="noopener noreferrer">
-                    <i className="fa-brands fa-whatsapp fa-beat"></i>
+                <a href="mailto:contacto@productoslatam.com" title="Email">
+                    <EnvelopeIcon className="h-6 w-6 text-white hover:text-blue-500" />
                 </a>
-                <a href="https://twitter.com/x" title="Twitter" target="_blank" rel="noopener noreferrer">
-                    <i className="fa-brands fa-x-twitter fa-beat"></i>
-                </a>
-                <a href="https://www.instagram.com/" title="Instagram" target="_blank" rel="noopener noreferrer">
-                    <i className="fa-brands fa-square-instagram fa-beat"></i>
-                </a>
-                <a href="https://ar.linkedin.com/" title="LinkedIn" target="_blank" rel="noopener noreferrer">
-                    <i className="fa-brands fa-linkedin fa-beat"></i>
+                <a href="tel:+1234567890" title="Teléfono">
+                    <PhoneIcon className="h-6 w-6 text-white hover:text-blue-600" />
                 </a>
             </div>
-            <a className="subir" href="#" title="Subir al inicio de la página">
-                <i className="fa-solid fa-chevron-up"></i>
-            </a>
         </footer>
     );
 }
-
--- components/button.js
+++ components/button.js
@@ -1,10 +1,10 @@
 function Button({ children, onClick }) {
     return (
-        <button 
+        <button
             type="button"
-            onClick={onClick} 
+            onClick={onClick}
             className="py-3.5 px-6 md:py-5 md:px-10 bg-blue-500 text-white font-bold text-base md:text-xl rounded-sm cursor-pointer shadow-md hover:opacity-50 focus:shadow-2xl border-none outline-none">
-            {children}            
+            {children}
         </button>
     );
 }
