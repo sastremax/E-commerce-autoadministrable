c57fdeefd5445bd4e76689926a70c985cf3cab18
SastreMax <sastremaximiliano@gmail.com>
2025-01-15 01:16:43 -0300

    CARRITO-CATALOGO-PAGE-IMAGENES
    . SE crea el carrito usando mambaui.
    . Se hacen modificaciones al catalogo que se usa de mambaui con tailwind.
    . Se maqueta la pagina principal para el acceso a multiples imagenes de productos a vender.

--- app/catalogo/page.js
+++ app/catalogo/page.js
@@ -62,24 +62,25 @@ function CatalogoPage() {
                     <h1 className="text-xl font-bold mb-6">Catálogo de Productos Electrónicos</h1>
                     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
                          {productos.map((producto) => (
-                              <div
+                              <Link
                                    key={producto.id}
-                                   className="p-4 border rounded-lg shadow hover:shadow-lg bg-white flex flex-col"
+                                   href={`/producto?id=${producto.id}`}
+                                   className="p-4 border rounded-lg shadow hover:shadow-lg bg-white cursor-pointer"
                               >
-                                   <img
-                                        src={producto.imagen}
-                                        alt={producto.nombre}
-                                        className="w-full h-48 object-contain rounded-lg mb-4"
-                                   />
-                                   <h2 className="text-lg font-semibold mb-2">{producto.nombre}</h2>
-                                   <p className="text-sm mb-4">{producto.descripcion}</p>
-                                   <div className="flex justify-between items-center mt-auto">
-                                        <span className="text-blue-600 font-bold">{producto.precio}</span>
-                                        <span className="text-gray-600 text-sm">
-                                             Stock: {producto.stock}
-                                        </span>
+                                   <div className="h-full flex flex-col">
+                                        <img
+                                             src={producto.imagen}
+                                             alt={producto.nombre}
+                                             className="w-full h-48 object-contain rounded-lg mb-4"
+                                        />
+                                        <h2 className="text-lg font-semibold mb-2">{producto.nombre}</h2>
+                                        <p className="text-sm mb-4">{producto.descripcion}</p>
+                                        <div className="mt-auto">
+                                             <span className="text-blue-600 font-bold mt-auto">{producto.precio}</span>
+                                             <p className="text-gray-700">Stock: {producto.stock}</p>
+                                        </div>
                                    </div>
-                              </div>
+                              </Link>
                          ))}
                     </div>
                </main>
--- app/page.js
+++ app/page.js
@@ -9,10 +9,24 @@ function HomePage() {
           >
                <div className="container mx-auto flex flex-col items-center justify-center text-center min-h-screen">
                     <div className="relative z-10 bg-white p-6 rounded-lg shadow-lg">
-                         <h1 className="text-4xl font-bold mb-4 text-black">Bienvenidos!</h1>
-                         <p className="text-lg text-gray-700 mb-4">
-                              Productos Latam tiene una amplia trayectoria en la venta de productos para el hogar.
-                         </p>
+                         <section className="py-6 bg-gray-100 text-gray-900">
+                              <div className="container grid grid-cols-2 gap-4 p-4 mx-auto md:grid-cols-4">
+                                   <img 
+                                        src="/images/ESTUDIANTES-TV.WEBP" 
+                                        alt="" 
+                                        className="w-full h-full col-span-2 row-span-2 rounded shadow-sm min-h-96 md:col-start-3 md:row-start-1 bg-gray-500 aspect-square"
+                                   />
+                                   <img alt="" className="w-full h-full rounded shadow-sm min-h-48 bg-gray-500 aspect-square" src="https://source.unsplash.com/random/200x200/?0" />
+                                   <img alt="" className="w-full h-full rounded shadow-sm min-h-48 bg-gray-500 aspect-square" src="https://source.unsplash.com/random/200x200/?1" />
+                                   <img alt="" className="w-full h-full rounded shadow-sm min-h-48 bg-gray-500 aspect-square" src="https://source.unsplash.com/random/200x200/?2" />
+                                   <img alt="" className="w-full h-full rounded shadow-sm min-h-48 bg-gray-500 aspect-square" src="https://source.unsplash.com/random/200x200/?3" />
+                                   <img alt="" className="w-full h-full rounded shadow-sm min-h-48 bg-gray-500 aspect-square" src="https://source.unsplash.com/random/200x200/?4" />
+                                   <img alt="" className="w-full h-full rounded shadow-sm min-h-48 bg-gray-500 aspect-square" src="https://source.unsplash.com/random/200x200/?5" />
+                                   <img alt="" className="w-full h-full rounded shadow-sm min-h-48 bg-gray-500 aspect-square" src="https://source.unsplash.com/random/200x200/?6" />
+                                   <img alt="" className="w-full h-full rounded shadow-sm min-h-48 bg-gray-500 aspect-square" src="https://source.unsplash.com/random/200x200/?7" />
+                                   <img src="https://source.unsplash.com/random/302x302/" alt="" className="w-full h-full col-span-2 row-span-2 rounded shadow-sm min-h-96 md:col-start-1 md:row-start-3 bg-gray-500 aspect-square" />
+                              </div>
+                         </section>
                     </div>
                </div>
           </div>
