15a009e93295d1b631e661ad30f05fe485fe8d9d
SastreMax <sastremaximiliano@gmail.com>
2025-01-31 15:10:02 -0300

    MODIFICAR y ELIMINAR PRODUCTOS
    . Se modifica admin/page.js con el estilo buscado. Falta pulir detalles visuales, pero funciona con la base de datos, creando, modificando y eliminando un producto.
    . Se modifica el componente ProductForm.js con las tres funcionalidades en buenas condiciones, falta pulir la estetica.

--- app/admin/page.js
+++ app/admin/page.js
@@ -30,9 +30,39 @@ const AdminPage = () => {
         fetchProductos();
     }, []);
 
+    const buttonText =
+        selectedAction === "add" ? "Crear Producto" :
+            selectedAction === "edit" ? "Actualizar Producto" :
+                selectedAction === "delete" ? "Eliminar Producto" :
+                    "";
+
+    const sectionText =
+        selectedAction === "add" ? "Complete los campos para agregar un producto." :
+            selectedAction === "edit" ? "Modifica los campos para actualizar el producto." :
+                selectedAction === "delete" ? "Confirma si deseas eliminar este producto." :
+                    "";
+
+    const handleDeleteProduct = async () => {
+        if (productId) {
+            try {
+                const productRef = doc(db, "productos", productId);
+                await deleteDoc(productRef);
+                Swal.fire("Producto Eliminado", "El producto fue eliminado", "success");
+
+                setProductos(productos.filter(producto => producto.id !== productId));
+                setProductId(null);
+                setSelectedAction("none");
+
+            } catch (error) {
+                console.error("Error deleting product: ", error);
+                Swal.fire("Error", "Hubo un problema al eliminar el producto.", "error");
+            }
+        }
+    };
+
     return (
         <section className="p-6 bg-gray-100 text-gray-900">
-            {selectedAction === "none" && (                
+            {selectedAction === "none" && (
                 <div className="flex flex-col items-center space-y-4">
                     <button
                         type="button"
@@ -66,7 +96,7 @@ const AdminPage = () => {
             {selectedAction !== "none" && (
                 <div className="space-y-4">
                     {selectedAction === "add" ? (
-                        <ProductForm selectedAction={selectedAction} productId={null} />
+                        <ProductForm selectedAction={selectedAction} productId={null} setSelectedAction={setSelectedAction} />
                     ) : (
                         <div>
                             <label htmlFor="productSelect" className="block text-sm font-medium text-gray-700">
@@ -85,10 +115,34 @@ const AdminPage = () => {
                                     </option>
                                 ))}
                             </select>
-                        </div>                
+                            {selectedAction !== "add" && productId && (
+                                <ProductForm selectedAction={selectedAction} productId={productId} setSelectedAction={setSelectedAction} />
+                            )}
+                        </div>
                     )}
                 </div>
-            )}        
+            )}
+            <div className="mt-6">
+                <p className="font-medium">{sectionText}</p>
+                <button
+                    type="button"
+                    className="bg-blue-500 text-white py-2 px-6 rounded mt-4"
+                    onClick={() => {
+                        if (selectedAction === "delete" && productId) {
+                            handleDeleteProduct();
+                        }
+                    }}
+                >
+                    {buttonText}
+                </button>
+                <button
+                    type="button"
+                    className="bg-gray-500 text-white py-2 px-6 rounded"
+                    onClick={() => setSelectedAction("none")}
+                >
+                    Volver Atrás
+                </button>
+            </div>
         </section>
     );
 }
