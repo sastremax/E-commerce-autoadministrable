d3a01cd07f0e7e8d4b5cb97c0749d3d03748d937
SastreMax <sastremaximiliano@gmail.com>
2025-02-16 20:12:27 -0300

    Button
    . Se hace mas flexible el componente para poder modificar los estilos con classname y por defecto es de tipo button

--- components/button.js
+++ components/button.js
@@ -1,7 +1,7 @@
-function Button({ children, onClick }) {
+function Button({ children, onClick, className, type = "button" }) {
     return (
         <button
-            type="button"
+            type={type}
             onClick={onClick}
             className="py-1.5 px-3 md:py-4 md:px-8 text-white text-sm bg-blue-600 font-bold rounded-lg border-black cursor-pointer"
         >
